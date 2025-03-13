eb8cb815fc4bffa83af884bd3befa9af5a8be717
Jake Leigh <jake.leigh@burberry.com>
2025-03-13 22:58:30 +0000

    define guesses and create board

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
+import axios from 'axios';
 
-function App() {
-  const [count, setCount] = useState(0)
+const WORD_LENGTH = 5;
+
+const Line = ({ guess }: LineProps) => {
+  const tiles = [];
+  for (let i = 0; i < WORD_LENGTH; i++) {
+    const char = guess[i];
+    tiles.push(
+      <div key={i} className="tile">
+        {char}
+      </div>,
+    );
+  }
+  return <div className="line">{tiles}</div>;
+};
+
+type LineProps = {
+  guess: string;
+};
+
+const App = () => {
+  const [word, setWord] = useState<string>('');
+  const [guesses, setGuesses] = useState<Array<null>>(new Array(6).fill(null));
+
+  const getRandomWord = async () => {
+    const res = await axios.get(
+      'https://random-word-api.vercel.app/api?words=1&length=5',
+    );
+    setWord(res.data);
+  };
+
+  useEffect(() => {
+    getRandomWord();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <div className="board">
+        {guesses.map((guess, index) => (
+          <Line key={index} guess={guess ?? ''} />
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
