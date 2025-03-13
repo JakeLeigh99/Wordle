39b5f33bb167e9acc72b967f5aa8497c83d4f622
Jake Leigh <jake@jakeleigh.io>
2025-03-13 23:48:05 +0000

    start implementing guess functionality with key event listeners

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,10 @@ type LineProps = {
 
 const App = () => {
   const [word, setWord] = useState<string>('');
-  const [guesses, setGuesses] = useState<Array<null>>(new Array(6).fill(null));
+  const [guesses, setGuesses] = useState<Array<string | null>>(
+    new Array(6).fill(null),
+  );
+  const [currGuess, setCurrentGuess] = useState<string>('');
 
   const getRandomWord = async () => {
     const res = await axios.get(
@@ -32,16 +35,26 @@ const App = () => {
     setWord(res.data);
   };
 
+  const handleKeyDown = (e: KeyboardEvent) => {
+    setCurrentGuess(currGuess + e.key);
+  };
+
   useEffect(() => {
     getRandomWord();
-  }, []);
+    window.addEventListener('keydown', handleKeyDown);
+    return () => window.removeEventListener('keydown', handleKeyDown);
+  }, [currGuess]);
 
   return (
     <div className="container">
       <div className="board">
-        {guesses.map((guess, index) => (
-          <Line key={index} guess={guess ?? ''} />
-        ))}
+        {guesses.map((guess, index) => {
+          const isCurrGuess = index === guesses.findIndex(val => val == null);
+          console.log('isCurrGuess', isCurrGuess);
+          return (
+            <Line key={index} guess={isCurrGuess ? currGuess : (guess ?? '')} />
+          );
+        })}
       </div>
     </div>
   );
